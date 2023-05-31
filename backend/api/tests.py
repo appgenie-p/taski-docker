f4532559a32fd8f6288510eefd786c083124a1b5
Gennadiy Polezhaev <gpolejaev@gmail.com>
2023-05-31 13:25:42 +0300

    tests added

---
+++ backend/api/tests.py
@@ -0,0 +1,28 @@
+# backend/api/tests.py
+from http import HTTPStatus
+
+from api import models
+from django.test import Client, TestCase
+
+
+class TaskiAPITestCase(TestCase):
+    """
+    Test case for the Taski API.
+
+    This test case contains tests for the Taski API, including testing the
+    creation and retrieval of tasks.
+    """
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_list_exists(self):
+        """Проверка доступности списка задач."""
+        response = self.guest_client.get('/api/tasks/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_task_creation(self):
+        """Проверка создания задачи."""
+        data = {'title': 'Test', 'description': 'Test'}
+        response = self.guest_client.post('/api/tasks/', data=data)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertTrue(models.Task.objects.filter(title='Test').exists())
